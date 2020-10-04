3055e754d2d13f7690ede0ead0c61ee407bdb98f
Peter van Diepen <pvdiepen@gmail.com>
2020-10-04 20:09:57 +0200

    Ranglijst met links naar speler.html

--- js/ranglijst.js
+++ js/ranglijst.js
@@ -2,7 +2,8 @@ const seizoenen = document.getElementById("seizoenen");
 seizoenen.appendChild(option("1819", "2018-2019"));
 seizoenen.appendChild(option("1920", "2019-2020"));
 seizoenen.appendChild(option("2021", "2020-2021"));
-let seizoen = seizoenen.value;
+const geenSeizoen = seizoenen.valueOf();
+let seizoen = geenSeizoen;
 
 const url = "http://localhost:3000/ranglijst/";
 
@@ -10,11 +11,10 @@ const tabel = document.getElementById("ranglijst");
 tabel.setAttribute("border", "1");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/location
+// https://www.geeksforgeeks.org/how-to-get-the-file-name-from-full-path-using-javascript/
+// alert("The URL of this page is: " + window.location.href);
 
-alert("The URL of this page is: " + window.location.href);
-const htmlBestanden = "/COOLfrontend";
-const link = document.getElementById("link");
-link.appendChild(naarSpeler(6212404, "ik zei de gek"));
+const htmlBestanden = "/COOLfrontend"; // TODO niet hard coderen
 
 function option(value, text) {
     let option = document.createElement('option');
@@ -29,7 +29,7 @@ function option(value, text) {
 function anderSeizoen() {
     geenRanglijst();
     seizoen = seizoenen.value;
-    if (seizoen != "0000") {
+    if (seizoen != geenSeizoen) {
         ranglijst();
     }
 }
@@ -40,7 +40,6 @@ function geenRanglijst() {
     }
 }
 
-
 // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
 
 function ranglijst() {
@@ -48,7 +47,8 @@ function ranglijst() {
         .then(response => response.json())
         .then(spelers => {
             spelers.map((speler, index) => {
-                tabel.appendChild(rij(index+1, speler.knsbNummer, speler.naam, speler.totaal));
+                // tabel.appendChild(rij(index+1, speler.naam, speler.totaal));
+                tabel.appendChild(rij(index+1, naarSpeler(speler.knsbNummer, speler.naam), speler.totaal));
             });
         });
 }
@@ -57,18 +57,22 @@ function rij(...kolommen) {
     let tr = document.createElement('tr');
     kolommen.map(kolom => {
         let td = document.createElement('td');
-        td.innerHTML = kolom;
+        if (kolom.nodeType === Node.ELEMENT_NODE) {
+            td.appendChild(kolom);
+        } else {
+            td.innerHTML = kolom;
+        }
         tr.appendChild(td);
     });
     return tr;
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Identifying_resources_on_the_Web
-// https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
 
 function naarSpeler(knsbNummer, naam) {
-    let link = document.createElement('a');
-    link.href = htmlBestanden + '/speler.html?seizoen=' + seizoen + '&speler=' + knsbNummer + '&naam=' + naam;
-    link.text = naam;
-    return link;
+    let a = document.createElement('a');
+    a.appendChild(document.createTextNode(naam));
+    a.title = naam;
+    a.href = htmlBestanden + '/speler.html?seizoen=' + seizoen + '&speler=' + knsbNummer + '&naam=' + naam;
+    return a;
 }
--- js/speler.js
+++ js/speler.js
@@ -1,3 +1,5 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+
 const params = (new URL(document.location)).searchParams;
 const naam = document.getElementById("naam");
 naam.innerHTML = params.get('naam');
