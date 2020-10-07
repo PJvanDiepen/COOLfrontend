2322a80c037a92daf9bc8edbf8b79c5d89f03db2
Peter van Diepen <pvdiepen@gmail.com>
2020-10-07 16:45:29 +0200

    eerste opzet met uitslagenlijst per speler

--- js/speler.js
+++ js/speler.js
@@ -3,3 +3,40 @@
 const params = (new URL(document.location)).searchParams;
 const naam = document.getElementById("naam");
 naam.innerHTML = params.get('naam');
+let seizoen = params.get('seizoen');
+let speler = params.get('speler');
+
+const url = "http://localhost:3000/uitslagen/";
+
+const tabel = document.getElementById("uitslagen");
+tabel.setAttribute("border", "1");
+uitslagenlijst();
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+
+//{"datum":"2018-09-10T22:00:00.000Z","rondeNummer":1,"witZwart":"z","naam":"Kiek Schouten","resultaat":"½","teamCode":"int","tegenstander":"","plaats":"Alkmaar","punten":7}
+
+function uitslagenlijst() {
+    console.log("ranglijst seizoen=" + seizoen);
+    fetch(url + seizoen + "/" + speler)
+        .then(response => response.json())
+        .then(uitslagen => {
+            uitslagen.map((u) => {
+                tabel.appendChild(rij(u.datum, u.rondeNummer, u.witZwart, u.naam, u.resultaat, u.teamCode, u.tegenstander, u.plaats, u.punten));
+            });
+        });
+}
+
+function rij(...kolommen) {
+    let tr = document.createElement('tr');
+    kolommen.map(kolom => {
+        let td = document.createElement('td');
+        if (kolom.nodeType === Node.ELEMENT_NODE) {
+            td.appendChild(kolom);  // ranglijst.js
+        } else {
+            td.innerHTML = kolom;
+        }
+        tr.appendChild(td);
+    });
+    return tr;
+}
