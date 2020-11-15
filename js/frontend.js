92cc23501ead298122b40941127909e747a3a389
Peter van Diepen <pvdiepen@gmail.com>
2020-11-15 20:57:04 +0100

    ronde.js en speler.js naar frontend.js en inline script in ronde.html en speler.html

--- js/frontend.js
+++ js/frontend.js
@@ -90,7 +90,6 @@ async function seizoenOpties(seizoenSelecteren, teamCode) {
         (team) => {
             seizoenSelecteren.appendChild(option(team.seizoen, seizoenVoluit(team.seizoen)));
         });
-    console.log(seizoenSelecteren.lastChild.value)
     if (seizoen !== null) {
         seizoenSelecteren.value = seizoen;
     }
@@ -103,16 +102,110 @@ function seizoenVoluit(seizoen) {
 
 function anderSeizoen() {
     localStorage.setItem("seizoen", seizoenSelecteren.value);
-    location.reload();
+    location.replace(url.pathname); // huidige html zonder searchParams
 }
 
-function ranglijst(tabel) {
-    document.getElementById("ranglijst").innerHTML = schaakVereniging + " " + seizoenVoluit(seizoen);
+function ranglijst(kop, lijst) {
+    kop.innerHTML = schaakVereniging + " " + seizoenVoluit(seizoen);
     asyncFetch("/ranglijst/" + seizoen,
         (speler, i) => {
-            tabel.appendChild(rij(
+            lijst.appendChild(rij(
                 i + 1,
                 naarSpeler(speler.knsbNummer,speler.naam),
                 speler.totaal));
         });
+}
+
+/*
+  -- uitslagen interne competitie per ronde
+  select
+      uitslag.knsbNummer,
+      wit.naam,
+      uitslag.tegenstanderNummer,
+      zwart.naam,
+      uitslag.resultaat
+  from uitslag
+  join persoon as wit on uitslag.knsbNummer = wit.knsbNummer
+  join persoon as zwart on uitslag.tegenstanderNummer = zwart.knsbNummer
+  where seizoen = @seizoen and teamCode = 'int' and rondeNummer = @rondeNummer and witZwart = 'w'
+  order by uitslag.seizoen, uitslag.bordNummer;
+   */
+function uitslagenRonde(kop, subkop, lijst) {
+    kop.innerHTML = schaakVereniging + " " + seizoenVoluit(seizoen);
+    subkop.innerHTML = rondeDatum + " ronde " + rondeNummer;
+    asyncFetch("/ronde/" + seizoen + "/" + rondeNummer,
+        (u) => {
+            lijst.appendChild(rij(
+                naarSpeler(u.knsbNummer, u.wit),
+                naarSpeler(u.tegenstanderNummer, u.zwart),
+                u.resultaat === "1" ? "1-0" : u.resultaat === "0" ? "0-1" : "½-½"));
+        });
+}
+
+/*
+ Fetch JSON uitslagen en verwerk die met uitslagRij tot rijen voor de uitslagen tabel.
+ */
+
+function uitslagenSpeler(kop, subkop, lijst) {
+    kop.innerHTML = schaakVereniging + " " + seizoenVoluit(seizoen);
+    subkop.innerHTML = naam;
+    let totaal = 300;
+    asyncFetch("/uitslagen/" + seizoen + "/" + speler,
+        (uitslag) => {
+            totaal = totaal + uitslag.punten;
+            lijst.appendChild(uitslagRij(uitslag, totaal));
+        });
+}
+
+/*
+Verwerk een JSON uitslag tot een rij van 8 kolommen.
+
+  -- punten van alle uitslagen per speler
+  select u.datum,
+      u.rondeNummer,
+      u.bordNummer,
+      u.witZwart,
+      u.tegenstanderNummer,
+      p.naam,
+      u.resultaat,
+      u.teamCode,
+      r.compleet,
+      r.uithuis,
+      r.tegenstander,
+      punten(@seizoen, @knsbNummer, u.teamCode, u.tegenstanderNummer, u.resultaat) as punten
+  from uitslag u
+  join persoon p on u.tegenstanderNummer = p.knsbNummer
+  join ronde r on u.seizoen = r.seizoen and u.teamCode = r.teamCode and u.rondeNummer = r.rondeNummer
+  where u.seizoen = @seizoen
+      and u.knsbNummer = @knsbNummer
+      and u.anderTeam = 'int'
+  order by u.datum, u.bordNummer;
+
+1. link naar interne ronde
+2. datum
+3. link naar interne tegenstander, link naar externe wedstrijd of andere tekst
+4. externe bord
+5. kleur
+6. resultaat
+7. punten
+8. voortschrijdend totaal
+
+@param u JSON uitslag
+@param totaal punten
+ */
+
+const TIJDELIJK_LID_NUMMER = 100;
+
+function uitslagRij(u, totaal) {
+    let datum = datumLeesbaar(u.datum);
+    if (u.tegenstanderNummer > TIJDELIJK_LID_NUMMER) {
+        return rij(naarRonde(u.rondeNummer, datum), datum, naarSpeler(u.tegenstanderNummer, u.naam), "", u.witZwart, u.resultaat, u.punten, totaal);
+    } else if (u.teamCode === "int") {
+        return rij(naarRonde(u.rondeNummer, datum), datum, u.naam, "", "", "", u.punten, totaal);
+    } else {
+        let thuis = u.uithuis === "t" ? teamVoluit(u.teamCode) : u.tegenstander;
+        let uit = u.uithuis === "u" ? teamVoluit(u.teamCode) : u.tegenstander;
+        let naarTeam = href(`${thuis} - ${uit}`,`team.html?team=${u.teamCode}&ronde=${u.rondeNummer}/#ronde${u.rondeNummer}`);
+        return rij("", datum, naarTeam, u.bordNummer, u.witZwart, u.resultaat, u.punten, totaal);
+    }
 }
